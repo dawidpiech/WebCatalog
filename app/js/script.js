d76cbb5d5f9e09bd3b02d5b4a0bc21e93a6ea30d
Dawid Piech <dawid.piech243@gmail.com>
2019-01-02 23:39:51 +0100

    Add Hamburger page, and some hamburgers animation

--- app/js/script.js
+++ app/js/script.js
@@ -1,52 +1,73 @@
-document.addEventListener("DOMContentLoaded", ()=>{
+document.addEventListener("DOMContentLoaded", () => {
 
     //MENU ANIMATION
     let hamburger = document.querySelector(".hamburger"),
-    menu = document.querySelector(".menu"),
-    nav = document.querySelector("nav")
+        menu = document.querySelector(".menu"),
+        nav = document.querySelector("nav")
 
     hamburger.addEventListener("click", (e) => {
         e.preventDefault()
         hamburger.classList.toggle("open")
-        if(menu.classList.contains("menu-close")){
+        if (menu.classList.contains("menu-close")) {
             menu.classList.remove("menu-close")
             menu.classList.add("menu-open")
             nav.classList.remove("close")
             nav.classList.add("open")
         }
-        else if(menu.classList.contains("menu-open")){
+        else if (menu.classList.contains("menu-open")) {
             menu.classList.add("menu-close")
             menu.classList.remove("menu-open")
             nav.classList.add("close")
             nav.classList.remove("open")
         }
-        else{
+        else {
             menu.classList.add("menu-open")
             nav.classList.add("open")
         }
-        
+
     })
 
     //LOADING ANIMATIONS - SOURCE
 
     let sourceBtn = document.querySelector(".view-source"),
-    sourceBtnClose = document.querySelector(".source-close-btn"),
-    sourceContainer = document.querySelector(".animations-source"),
-    arrowLeft = document.querySelector(".arrow-left"),
-    arrowRight = document.querySelector(".arrow-right"),
-    counter = 1,
-    sizeAnimations = [120, 140, 290, 140, 120, 200, 440, 240],
-    animationsContainer = document.querySelector(".animations"),
-    animationHandlers = document.querySelectorAll(".animations > ul > li"),
-    animationSourceHandlers = document.querySelectorAll(".animate-source"),
-    animationsBackground = document.querySelector(".loading-animations-container")
+        sourceBtnClose = document.querySelector(".source-close-btn"),
+        sourceContainer = document.querySelector(".animations-source"),
+        arrowLeft = document.querySelector(".arrow-left"),
+        arrowRight = document.querySelector(".arrow-right"),
+        counter = 1,
+        sizeAnimations = [120, 140, 290, 140, 120, 200, 440, 240],
+        animationsContainer = document.querySelector(".animations"),
+        animationHandles = document.querySelectorAll(".animations > ul > li"),
+        animationSourceHandles = document.querySelectorAll(".animate-source"),
+        animationsBackground = document.querySelector(".loading-animations-container")
 
-    sourceBtn.addEventListener("click", openSource)
-    sourceBtnClose.addEventListener("click", closeSource)
-    arrowRight.addEventListener("click", nextAnimation)
-    arrowLeft.addEventListener("click", beforeAnimation)
+        try {
+            sourceBtn.addEventListener("click", openSource)
+            sourceBtnClose.addEventListener("click", closeSource)
+            arrowRight.addEventListener("click", nextAnimation)
+            arrowLeft.addEventListener("click", beforeAnimation)
+        } catch (error) {
+            
+        }
+    
 
-    function openSource(e){
+    /*************************************************
+     * HAMBURGERS ANIMATIONS
+     */
+
+    let hamburger_3 = document.querySelector(".hamburger-3 div"),
+    hamburger_4 = document.querySelector(".hamburger-4 div")
+    
+
+    hamburger_3.addEventListener("mouseover", () => hamburger_3.classList.toggle("active"))
+    hamburger_3.addEventListener("mouseout", () => hamburger_3.classList.toggle("active"))
+
+    hamburger_4.addEventListener("mouseover", () => hamburger_4.classList.add("active"))
+    hamburger_4.addEventListener("mouseout", () => hamburger_4.classList.remove("active"))
+    hamburger_4.addEventListener("mouseover", () => hamburger_4.classList.remove("unactive"))
+    hamburger_4.addEventListener("mouseout", () => hamburger_4.classList.add("unactive"))
+    
+    function openSource(e) {
         e.preventDefault()
         sourceContainer.style.display = "block"
         sourceContainer.classList.remove("source-close")
@@ -57,12 +78,12 @@ document.addEventListener("DOMContentLoaded", ()=>{
         arrowLeft.style.display = "none"
     }
 
-    function closeSource(e){
+    function closeSource(e) {
         e.preventDefault()
         sourceContainer.classList.remove("source-open")
         sourceContainer.classList.add("source-close")
         animationsContainer.classList.remove("animations-active-source")
-        
+
         setTimeout(() => {
             sourceContainer.style.display = "none"
             hamburger.style.display = "block"
@@ -71,46 +92,43 @@ document.addEventListener("DOMContentLoaded", ()=>{
         }, 800)
     }
 
-    function nextAnimation(e){
+    function nextAnimation(e) {
         e.preventDefault()
 
         let a = 0
         for (let index = 0; index <= counter; index++) {
-            (index===counter) ? a += (sizeAnimations[index])/2 : a += sizeAnimations[index]
+            (index === counter) ? a += (sizeAnimations[index]) / 2 : a += sizeAnimations[index]
         }
-        animationHandlers.item(counter-1).classList.remove("selected")
-        animationHandlers.item(counter).classList.add("selected")
+        animationHandles.item(counter - 1).classList.remove("selected")
+        animationHandles.item(counter).classList.add("selected")
         animationsContainer.style.transform = `translateX(-${a}px) translateY(-50%)`
-        animationSourceHandlers.item(counter-1).style.display = "none"
-        animationSourceHandlers.item(counter).style.display = "block"
+        animationSourceHandles.item(counter - 1).style.display = "none"
+        animationSourceHandles.item(counter).style.display = "block"
         animationsBackground.classList.remove(`active${counter}`)
-        animationsBackground.classList.add(`active${counter+1}`)
+        animationsBackground.classList.add(`active${counter + 1}`)
         counter++
-        (counter>=animationHandlers.length) ? arrowRight.style.display = "none" : arrowLeft.style.display = "block"
+        (counter >= animationHandles.length) ? arrowRight.style.display = "none" : arrowLeft.style.display = "block"
 
     }
 
-    function beforeAnimation(e){
+    function beforeAnimation(e) {
         e.preventDefault()
         counter--
         let a = 0
         for (let index = 1; index <= counter; index++) {
-            (index===counter) ? a += (sizeAnimations[index-1])/2 : a += sizeAnimations[index-1]
+            (index === counter) ? a += (sizeAnimations[index - 1]) / 2 : a += sizeAnimations[index - 1]
         }
-        animationHandlers.item(counter).classList.remove("selected")
-        animationHandlers.item(counter-1).classList.add("selected")
+        animationHandles.item(counter).classList.remove("selected")
+        animationHandles.item(counter - 1).classList.add("selected")
         animationsContainer.style.transform = `translateX(-${a}px) translateY(-50%)`
-        animationSourceHandlers.item(counter).style.display = "none"
-        animationSourceHandlers.item(counter-1).style.display = "block"
-        animationsBackground.classList.remove(`active${counter+1}`)
+        animationSourceHandles.item(counter).style.display = "none"
+        animationSourceHandles.item(counter - 1).style.display = "block"
+        animationsBackground.classList.remove(`active${counter + 1}`)
         animationsBackground.classList.add(`active${counter}`)
         console.log(counter)
-        if(counter<=1)
-            arrowLeft.style.display = "none" 
-        else 
+        if (counter <= 1)
+            arrowLeft.style.display = "none"
+        else
             arrowRight.style.display = "block"
     }
-
-
-
 })
