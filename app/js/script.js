d895ce041c70e023a7e9d1e06b985acc623a4181
Dawid Piech <dawid.piech243@gmail.com>
2019-01-14 15:33:20 +0100

    Fix js bugs

--- app/js/script.js
+++ app/js/script.js
@@ -41,67 +41,70 @@ document.addEventListener("DOMContentLoaded", () => {
         animationSourceHandles = document.querySelectorAll(".animate-source"),
         animationsBackground = document.querySelector(".loading-animations-container")
 
-        try {
-            sourceBtn.addEventListener("click", openSource)
-            sourceBtnClose.addEventListener("click", closeSource)
-            arrowRight.addEventListener("click", nextAnimation)
-            arrowLeft.addEventListener("click", beforeAnimation)
-        } catch (error) {
-            
-        }
-    
+    try {
+        sourceBtn.addEventListener("click", openSource)
+        sourceBtnClose.addEventListener("click", closeSource)
+        arrowRight.addEventListener("click", nextAnimation)
+        arrowLeft.addEventListener("click", beforeAnimation)
+    } catch (error) {
+
+    }
+
 
     /*************************************************
      * HAMBURGERS ANIMATIONS
      */
 
     let hamburger_3 = document.querySelector(".hamburger-3 div"),
-    hamburger_4 = document.querySelector(".hamburger-4 div"),
-    hamburger_5 = document.querySelector(".hamburger-5 div"),
-    hamburger_6 = document.querySelector(".hamburger-6 div"),
-    hamburger_7 = document.querySelector(".hamburger-7 div"),
-    hamburger_8 = document.querySelector(".hamburger-8 div"),
-    hamburger_9 = document.querySelector(".hamburger-9 div")
+        hamburger_4 = document.querySelector(".hamburger-4 div"),
+        hamburger_5 = document.querySelector(".hamburger-5 div"),
+        hamburger_6 = document.querySelector(".hamburger-6 div"),
+        hamburger_7 = document.querySelector(".hamburger-7 div"),
+        hamburger_8 = document.querySelector(".hamburger-8 div"),
+        hamburger_9 = document.querySelector(".hamburger-9 div")
 
 
-    
 
-    hamburger_3.addEventListener("mouseover", () => hamburger_3.classList.add("active"))
-    hamburger_3.addEventListener("mouseout", () => hamburger_3.classList.remove("active"))
-    hamburger_3.addEventListener("mouseover", () => hamburger_3.classList.remove("unactive"))
-    hamburger_3.addEventListener("mouseout", () => hamburger_3.classList.add("unactive"))
+    try {
+        hamburger_3.addEventListener("mouseover", () => hamburger_3.classList.add("active"))
+        hamburger_3.addEventListener("mouseout", () => hamburger_3.classList.remove("active"))
+        hamburger_3.addEventListener("mouseover", () => hamburger_3.classList.remove("unactive"))
+        hamburger_3.addEventListener("mouseout", () => hamburger_3.classList.add("unactive"))
 
+        hamburger_4.addEventListener("mouseover", () => hamburger_4.classList.add("active"))
+        hamburger_4.addEventListener("mouseout", () => hamburger_4.classList.remove("active"))
+        hamburger_4.addEventListener("mouseover", () => hamburger_4.classList.remove("unactive"))
+        hamburger_4.addEventListener("mouseout", () => hamburger_4.classList.add("unactive"))
 
-    hamburger_4.addEventListener("mouseover", () => hamburger_4.classList.add("active"))
-    hamburger_4.addEventListener("mouseout", () => hamburger_4.classList.remove("active"))
-    hamburger_4.addEventListener("mouseover", () => hamburger_4.classList.remove("unactive"))
-    hamburger_4.addEventListener("mouseout", () => hamburger_4.classList.add("unactive"))
+        hamburger_5.addEventListener("mouseover", () => hamburger_5.classList.add("active"))
+        hamburger_5.addEventListener("mouseout", () => hamburger_5.classList.remove("active"))
+        hamburger_5.addEventListener("mouseover", () => hamburger_5.classList.remove("unactive"))
+        hamburger_5.addEventListener("mouseout", () => hamburger_5.classList.add("unactive"))
 
-    hamburger_5.addEventListener("mouseover", () => hamburger_5.classList.add("active"))
-    hamburger_5.addEventListener("mouseout", () => hamburger_5.classList.remove("active"))
-    hamburger_5.addEventListener("mouseover", () => hamburger_5.classList.remove("unactive"))
-    hamburger_5.addEventListener("mouseout", () => hamburger_5.classList.add("unactive"))
+        hamburger_6.addEventListener("mouseover", () => hamburger_6.classList.add("active"))
+        hamburger_6.addEventListener("mouseout", () => hamburger_6.classList.remove("active"))
+        hamburger_6.addEventListener("mouseover", () => hamburger_6.classList.remove("unactive"))
+        hamburger_6.addEventListener("mouseout", () => hamburger_6.classList.add("unactive"))
 
-    hamburger_6.addEventListener("mouseover", () => hamburger_6.classList.add("active"))
-    hamburger_6.addEventListener("mouseout", () => hamburger_6.classList.remove("active"))
-    hamburger_6.addEventListener("mouseover", () => hamburger_6.classList.remove("unactive"))
-    hamburger_6.addEventListener("mouseout", () => hamburger_6.classList.add("unactive"))
+        hamburger_7.addEventListener("mouseover", () => hamburger_7.classList.add("active"))
+        hamburger_7.addEventListener("mouseout", () => hamburger_7.classList.remove("active"))
+        hamburger_7.addEventListener("mouseover", () => hamburger_7.classList.remove("unactive"))
+        hamburger_7.addEventListener("mouseout", () => hamburger_7.classList.add("unactive"))
 
-    hamburger_7.addEventListener("mouseover", () => hamburger_7.classList.add("active"))
-    hamburger_7.addEventListener("mouseout", () => hamburger_7.classList.remove("active"))
-    hamburger_7.addEventListener("mouseover", () => hamburger_7.classList.remove("unactive"))
-    hamburger_7.addEventListener("mouseout", () => hamburger_7.classList.add("unactive"))
+        hamburger_8.addEventListener("mouseover", () => hamburger_8.classList.add("active"))
+        hamburger_8.addEventListener("mouseout", () => hamburger_8.classList.remove("active"))
+        hamburger_8.addEventListener("mouseover", () => hamburger_8.classList.remove("unactive"))
+        hamburger_8.addEventListener("mouseout", () => hamburger_8.classList.add("unactive"))
 
-    hamburger_8.addEventListener("mouseover", () => hamburger_8.classList.add("active"))
-    hamburger_8.addEventListener("mouseout", () => hamburger_8.classList.remove("active"))
-    hamburger_8.addEventListener("mouseover", () => hamburger_8.classList.remove("unactive"))
-    hamburger_8.addEventListener("mouseout", () => hamburger_8.classList.add("unactive"))
+        hamburger_9.addEventListener("mouseover", () => hamburger_9.classList.add("active"))
+        hamburger_9.addEventListener("mouseout", () => hamburger_9.classList.remove("active"))
+        hamburger_9.addEventListener("mouseover", () => hamburger_9.classList.remove("unactive"))
+        hamburger_9.addEventListener("mouseout", () => hamburger_9.classList.add("unactive"))
+    }
+    catch{
 
-    hamburger_9.addEventListener("mouseover", () => hamburger_9.classList.add("active"))
-    hamburger_9.addEventListener("mouseout", () => hamburger_9.classList.remove("active"))
-    hamburger_9.addEventListener("mouseover", () => hamburger_9.classList.remove("unactive"))
-    hamburger_9.addEventListener("mouseout", () => hamburger_9.classList.add("unactive"))
-    
+    }
+
     function openSource(e) {
         e.preventDefault()
         sourceContainer.style.display = "block"
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,3 +1,5 @@
+// npm install --save-dev gulp@3.9.1 del gulp-sass gulp-clean-css gulp-autoprefixer gulp-concat gulp-uglify gulp-sourcemaps browser-sync gulp-babel @babel/core @babel/preset-env
+
 import gulp from 'gulp'
 import del from 'del'
 import sass from 'gulp-sass'
@@ -11,44 +13,44 @@ let browserSync = require('browser-sync').create()
 gulp.task('default', ['build'])
 
 //Minify and concat js file
-gulp.task('js', ()=>{
+gulp.task('js', () => {
   gulp.src('./app/js/*.js')
-  .pipe(sourcemaps.init())
-  .pipe(concat('main.min.js'))
-  .pipe(uglify().on('error', console.log))
-  .pipe(sourcemaps.write())
-  .pipe(gulp.dest('./dist/js'))
-  .pipe(browserSync.reload({
-    stream: true
-  }))
+    .pipe(sourcemaps.init())
+    .pipe(concat('main.min.js'))
+    .pipe(uglify().on('error', console.log))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./dist/js'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
 })
 
 //compile sass file
-gulp.task('sass', ()=>{
+gulp.task('sass', () => {
   gulp.src('./app/sass/*.scss')
-  .pipe(sourcemaps.init())
-  .pipe(sass().on('error', sass.logError))
-  .pipe(autoprefixer({
-    grid: true,
-    browsers: ['last 2 version']
-  }))
-  .pipe(clean())
-  .pipe(sourcemaps.write())
-  .pipe(gulp.dest('./dist/css'))
-  .pipe(browserSync.reload({
-    stream: true
-  }))
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+      grid: true,
+      browsers: ['last 2 version']
+    }))
+    .pipe(clean())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./dist/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
 });
 
 //Copy static files
 gulp.task('static', () => {
   gulp.src('app/**/*.html')
-  .pipe(gulp.dest('dist'))
+    .pipe(gulp.dest('dist'))
   gulp.src('app/assets/**/*.*')
-  .pipe(gulp.dest('dist/assets'))
-  .pipe(browserSync.reload({
-    stream: true
-  }))
+    .pipe(gulp.dest('dist/assets'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
 });
 
 
@@ -57,19 +59,19 @@ gulp.task('clean', () => {
   return del.sync('dist');
 });
 
-gulp.task('build', ['clean'], ()=>{
+gulp.task('build', ['clean'], () => {
   gulp.start(['static', 'sass', 'js'])
 });
 
-gulp.task('browserSync', function() {
+gulp.task('browserSync', function () {
   browserSync.init({
-      server: {
-          baseDir: "./dist", 
-      }
+    server: {
+      baseDir: "./dist",
+    }
   });
 });
 
-gulp.task('watch', ['default', 'browserSync'], () =>{
+gulp.task('watch', ['default', 'browserSync'], () => {
   gulp.watch('./app/sass/*.scss', ['sass'], browserSync.reload)
   gulp.watch('./app/js/**/*.js', ['js'], browserSync.reload)
   gulp.watch('./app/**/*.html', ['static'], browserSync.reload)
